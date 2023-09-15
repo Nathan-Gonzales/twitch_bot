de54884ae61ce02e42b7271c9e909b3fe74a0e05
Nathan-Gonzales <nathangonzales114@yahoo.com>
2023-09-15 02:38:45 -0600

    try again

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+
+const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
+
+const commands = {
+    website: {
+        response: 'https://spacejelly.dev'
+    },
+    upvote: {
+        response: (user) => `User ${user} was just upvoted!`
+    },
+}
+
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN
+	},
+	channels: [ 'nathanieljenkins' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+    const [raw, command, argument] = message.match(regexpCommand);
+
+    const {response} = commands[command] || {};
+
+    if (typeof response === 'function') {
+        client.say(channel, response(tags.username));
+    }
+    else if(typeof response === 'string') {
+        client.say(channel, response);
+    }
+
+	// if(message.toLowerCase() === '!hello') {
+	// 	// "@alca, heya!"
+	// 	client.say(channel, `@${tags.username}, heya!`);
+	// }
+});
+			
